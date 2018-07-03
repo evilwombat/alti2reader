f9d4a25fcd8e7272dfcd2679f0095b66a289574b
evilwombat <evilwombat@server.fake>
2018-07-03 00:20:39 -0700

    Alti2Reader: Bump version prior to release
    We've made some interesting changes, so bump the program
    version to 1.3.0.0 and update the copyright in a small way.
    
    Signed-off-by: evilwombat <evilwombat@server.fake>

--- Alti2Reader/Properties/AssemblyInfo.cs
+++ Alti2Reader/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lobanov'Software")]
 [assembly: AssemblyProduct("Alti-2 Reader")]
-[assembly: AssemblyCopyright("Copyright © Alexey Lobanov 2011")]
+[assembly: AssemblyCopyright("Copyright © Alexey Lobanov 2011, with modifications by evilwombat © 2018")]
 [assembly: AssemblyTrademark("Alti-2 Reader")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("")]
